490482d18fab88fc99873e76efd35f60eebdf1ea
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2022-05-10 01:21:20 +0200

    made changes

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,7 +1,10 @@
 module.exports = {
    name: 'messageDelete',
    execute(message, client) {
-      message.guild.channels.cache.find(c => c.name == 'espionage').send(
+      if (message.guild.id != 553933942193913856)
+         return;
+
+      message.guild.channels.cache.find(c => c.name == 'espionage')?.send(
       `A message has been deleted.\n` +
       `Author: ${message.author}\n` +
       `Channel: ${message.channel}\n` +
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -3,14 +3,17 @@ const C = require("../modules/common.js");
 module.exports = {
    name: 'messageUpdate',
    execute(oldMessage, newMessage, client) {
-      newMessage.guild.channels.cache.find(c => c.name == 'espionage').send(
+      if (newMessage.guild.id != 553933942193913856)
+         return;
+
+      newMessage.guild.channels.cache.find(c => c.name == 'espionage')?.send(
       `A message has been edited.\n` +
       `Author: ${oldMessage.author}\n` +
       `Channel: ${oldMessage.channel}\n` +
       `Old message: ${oldMessage}\n` +
       `New message: ${newMessage}\n` +
       `--------------------------------------------------`);
-      
+
       C.dcValidateForBannedWords(newMessage);
    },
 };
