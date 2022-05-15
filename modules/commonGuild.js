119ab8eb98dcf019374ca0995c1e1dd65a4655f6
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2022-05-15 20:32:14 +0200

    made changes

--- modules/commonGuild.js
+++ modules/commonGuild.js
@@ -145,7 +145,7 @@ module.exports.transferCurrency = transferCurrency;
 //----------------------------------------------------------- RECORD ----------------------------------------------------------
 // OK---------------------------------------------------------------------------------------------------------------
 async function getRecordDoc() {
-   return await DB.findOne(SG.record, {}) ?? new SG.record();
+   return await DB.findOne(SG.record, {}) ?? new SG.record({});
 }
 
 module.exports.getCurrencyObject = getCurrencyObject;
