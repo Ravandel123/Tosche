cfc2b39ceae1b940a8b1ec74f246be8eedf26f98
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2022-07-03 00:45:54 +0200

    made changes

--- modules/arraysBWD.js
+++ modules/arraysBWD.js
@@ -388,7 +388,8 @@ const chapter4 = {
       `https://www.westerndeep.net/wp-content/uploads/2022/05/4-66.jpg`,
       `https://www.westerndeep.net/wp-content/uploads/2022/05/4-67.jpeg`,
       `https://www.westerndeep.net/wp-content/uploads/2022/06/4-68.jpg`,
-      `https://www.westerndeep.net/wp-content/uploads/2022/06/4-69.jpeg`
+      `https://www.westerndeep.net/wp-content/uploads/2022/06/4-69.jpeg`,
+      `https://www.westerndeep.net/wp-content/uploads/2022/07/4-70.jpeg`
    ],
    regularUrl: `chapter-4-page-`,
    exceptions: new Map([
