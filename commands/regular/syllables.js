66b15e3ffc9c788765e739ca4bed8092c6484259
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2022-07-15 23:18:22 +0200

    made changes

---
+++ commands/regular/syllables.js
@@ -0,0 +1,17 @@
+const C = require('../../modules/common.js');
+const CC = require('../../modules/commonCommands.js');
+
+module.exports = {
+   name: 'syllables',
+   description: 'Returns the amount of syllables in the word.',
+   usage: '[word]',
+   example: 'deltrada',
+   execute(message, args) {
+      const requiredArgs = ['a word'];
+
+      if (!CC.checkArgsAmount(message, args, requiredArgs))
+         return;
+
+      C.dcRespondToMsg(message, `The word '${args[1]}' has ${C.strGetSyllablesAmount(args[1]) syllable(s).`);
+   },
+}
