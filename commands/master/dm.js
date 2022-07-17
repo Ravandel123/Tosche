61f523d693d8410fcc7625a0adc8c513a045aa15
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2022-07-17 16:42:47 +0200

    made changes

--- commands/master/dm.js
+++ commands/master/dm.js
@@ -6,8 +6,11 @@ module.exports = {
    usage: '[user ID] [message content]',
    example: '554696495190769692 Welcome to Deltrada',
    execute(message, args) {
-      C.dcSendDM(message, args[1], message.content.slice(args[1].length + 1));
-      console.log(message.content);
+      C.dcSendDM(message, args[1], message.content.slice(args[1]?.length + 1));
+      console.log('0= ' + args[0]);
+      console.log('1= ' + args[1]);
+      console.log('2= ' + args[2]);
+      console.log('3= ' + args[3]);
       // message.client.users.cache.get(arguments[1])
       // .send(message.content.slice(Config.prefix.length + command.length + arguments[1].length + 1))
    },
