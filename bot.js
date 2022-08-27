9805960eff02eb995bf86a729b3667beffa4bf5f
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2022-08-27 20:10:06 +0200

    made changes

--- bot.js
+++ bot.js
@@ -7,6 +7,15 @@ const CRON = require('node-cron');
 const DB = require('./modules/db.js');
 const CG = require('./modules/commonGuild.js');
 
+console.log(MONGODB_URI);
+console.log(MONGODB_PASSWORD);
+
+let x = {
+   a: xd;
+}
+
+x.map(e => e > 2);
+
 
 const client = new Client({
    intents: [
